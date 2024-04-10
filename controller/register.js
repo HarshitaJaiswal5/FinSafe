79727a6a2ec6c0ff298bd169f67a8742cd5b1296
[Eklavya Singh] <[eklavyasinghparihar7875@gmail.com]>
2024-04-10 14:16:56 +0530

    fix-transaction-api(W)

--- controller/register.js
+++ controller/register.js
@@ -119,23 +119,21 @@ async function sendMail(){
 
 const transaction = async (req, res) => {
     try {
-        const { Account_no,Transaction_type , Amount} = req.body;
-        const result = await validateFields_trans(req.body);
+        const { Account_Number,Transaction_type,Amount} = req.body;
 
-        if (!result) {
-            return res.status(400).send("Only valid fields to add: 'account_no', 'transaction_id', 'amount', 'transaction_type', 'transaction_description'")
-        } else {
-            
+
+
 
 
 
 if(Transaction_type === "debit"){
+
 const transactionSuccess = await db.transaction(async (trx)=>{
-await trx('Account_balance')
-    .where({ Account_no })
-    .decrement('Total_balance',Amount)
-await db('transaction_details').insert({
-    Account_no,
+await trx('Account_Balance')
+    .where({ Account_Number })
+    .decrement('Total_Balance',Amount)
+await db('Transaction_detailss').insert({
+    Account_no:Account_Number,
     Transaction_type , 
     Amount,
     created_at: new Date()
@@ -145,12 +143,13 @@ await db('transaction_details').insert({
 
 
 if(Transaction_type === "credit"){
+
 const transactionSuccess = await db.transaction(async (trx)=>{
-await trx('Account_balance')
-    .where({ Account_no })
-    .increment('Total_balance',Amount)
-await db('transaction_details').insert({
-    Account_no,
+await trx('Account_Balance')
+    .where({ Account_Number })
+    .increment('Total_Balance',Amount)
+await db('Transaction_details').insert({
+    Account_no:Account_Number,
     Transaction_type , 
     Amount,
     created_at: new Date()
@@ -158,75 +157,12 @@ await db('transaction_details').insert({
  })
 }
 
-}
 
-
-        const balance = await db.select('balance').from('note_denominations').where('account_no', '=', account_no);
-        const actBalance = (balance[0].balance);
-
-
-        const t_id = await db.select('transaction_id').from('transaction_details').where('account_no', '=', account_no);
-        console.log(t_id)
-        console.log(t_id.length)
-
-        if (t_id.length <= 1) {
-            await db('current_balance_details').insert({
-                account_no
-            })
-            if (transaction_type == 'debit') {
-
-                const result = actBalance - amount;
-                console.log(result);
-                const final = await db('current_balance_details')
-                    .update({
-                        current_balance: result,
-                    })
-                    .where('account_no', '=', account_no)
-            } else if (transaction_type == 'credit') {
-
-                const result = actBalance + amount;
-                console.log(result);
-
-
-
-                const final = await db('current_balance_details')
-                    .update({
-                        current_balance: result,
-                    })
-                    .where('account_no', '=', account_no);
-            }
-        } else {
-            const cur_balance = await db.select('current_balance').from('current_balance_details').where('account_no', '=', account_no);
-            const act_cur_balance = (cur_balance[0].current_balance);
-            console.log(`act_cur_balance`);
-            if (transaction_type == 'debit') {
-                const result = act_cur_balance - amount;
-                console.log(result);
-                await db('current_balance_details')
-                    .update({
-                        current_balance: result,
-                    })
-                    .where('account_no', '=', account_no)
-            } else if (transaction_type == 'credit') {
-
-                const result = act_cur_balance + amount;
-                console.log(result);
-                await db('current_balance_details')
-
-                    .update({
-                        current_balance: result,
-                    })
-                    .where('account_no', '=', account_no)
-
-
-            }
-
-        }
-        res.status(201).send("Transaction registered successfully!");
-    } catch (error) {
-        console.log(error);
+res.status(201).send("Transaction registered successfully!");
+ } catch (error) {
+console.log(error);
     }
-};
+}
 const denoms = async (req, res) => {
     try {
         const { account_no, notes_1000rs, notes_2000rs } = req.body;
